e69f745cbb770c61c619535e4cb54f1aab611625
ko-maru <kokitechno@gmail.com>
2022-05-20 08:41:17 +0900

    add buffers list to tabline

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -8,16 +8,29 @@ require('lualine').setup {
   sections = {
     lualine_a = { 'mode' },
     lualine_b = {
-      'branch', 
-      'diff', 
-      { 
+      'branch',
+      'diff',
+      {
         'diagnostics',
         sources = { 'nvim_lsp' }
-      } 
+      }
     },
     lualine_c = { 'filename' },
     lualine_x = { 'encoding', 'fileformat', 'filetype' },
     lualine_y = { 'progress' },
     lualine_z = { 'location' },
+  },
+  tabline = {
+    lualine_a = {
+      {
+        'buffers',
+        mode = 2,
+        max_length = vim.o.columns
+      }
+    },
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {}, lualine_z = {},
   }
-} 
+}
